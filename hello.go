b2f92aa79ce8e104a6ba46f418624e314d939ed3
Allan Chau <achau@infoblox.com>
2017-05-09 14:06:02 -0700

    Added a package

--- hello.go
+++ hello.go
@@ -1,9 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"addy-add-add"
+)
 
 func main() {
 	const World = "世界"
+	int sum;
 	fmt.Printf("Hello world!\n")
 	fmt.Println("Hello", World)
+	sum = AddyAddAdd(1, 2)
+	fmt.Println("Sum1 = ", sum)
+	fmt.Println("Sum2 = ", AddyAddAdd(sum, 4))
 }
